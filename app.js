80f8b39d517542ce9e15b16eb46bb39adb3a2759
Matthew Gazzera <genzu44@gmail.com>
2021-07-21 14:34:24 -0700

    Added functionality to button and retireves data from api

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 // API KEY: FsaBzi92usSGPy8hdKrt7t16Z79nAYFW
 
-const gifStaging = $('gif-staging');
-const search = $('search');
+const gifStaging = document.querySelector('#gif-staging');
+const form = document.querySelector('#form');
+const search = document.querySelector('#search');
+
+// Retrieve GIF from Giphy
+
+const getGif = async function (e) {
+     e.preventDefault();
+
+     const res = await axios.get('http://api.giphy.com/v1/gifs/search', {
+          params: {
+               q: search.value,
+               api_key: 'FsaBzi92usSGPy8hdKrt7t16Z79nAYFW',
+          },
+     });
+     console.log(res);
+     search.value = '';
+};
+
+// Add functionality to submit
+
+form.addEventListener('submit', getGif);
